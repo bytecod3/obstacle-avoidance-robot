1ee990547679069dec9028762dbddf737bbeaa48
ObaraEmmanuel <emmanuelobarany@gmail.com>
2022-11-24 14:28:08 +0300

    Implement fast ping

--- src/fast_ping.cpp
+++ src/fast_ping.cpp
@@ -1,5 +1,7 @@
 #include "fast_ping.h"
 
+FastPing* FastPing::_instance = nullptr;
+
 void left_echo_isr(){
     FastPing* ping = FastPing::getDefaultInstance();
     ping->recv_left();
@@ -16,7 +18,7 @@ void front_echo_isr(){
 }
 
 
-FastPing::FastPing(uint8_t trigger, uint8_t left_echo_pin, uint8_t right_echo_pin, uint8_t front_echo_pin): _trigger(trigger), _left_pin(left_echo_pin), _right_pin(right_echo_pin){
+FastPing::FastPing(uint8_t trigger, uint8_t left_echo_pin, uint8_t right_echo_pin, uint8_t front_echo_pin): _trigger(trigger){
     if(_instance == nullptr)
         _instance = this;
     attachInterrupt(left_echo_pin, left_echo_isr, FALLING);
@@ -25,14 +27,20 @@ FastPing::FastPing(uint8_t trigger, uint8_t left_echo_pin, uint8_t right_echo_pi
 }
 
 
-void FastPing::waitForAll(){
+uint8_t FastPing::waitForAll(){
     pingAll();
-
     while ((micros() - _start_time) < TIME_OUT)
     {
         if(_left_sensor != -1 && _right_sensor != -1 && _front_sensor != -1)
             break;
     }
+
+    uint8_t d_flag = 0;
+    if(_front_sensor > KEEP_OUT_TIME)d_flag |= FRONT;
+    if(_left_sensor > KEEP_OUT_TIME)d_flag |= LEFT;
+    if(_right_sensor > KEEP_OUT_TIME)d_flag |= RIGHT;
+
+    return d_flag;
 }
 
 void FastPing::pingAll(){
@@ -42,21 +50,35 @@ void FastPing::pingAll(){
     digitalWrite(_trigger, 1);
 	delayMicroseconds(10);
 	digitalWrite(_trigger, 0);
-    _start_time = micros();
+    _start_time = (long)micros();
 }
 
 void FastPing::recv_left(){
-    _left_sensor = micros();
+    _left_sensor = (long)micros() - _start_time;
 }
 
 void FastPing::recv_right(){
-    _right_sensor = micros();
+    _right_sensor = (long)micros() - _start_time;
 }
 
 void FastPing::recv_front(){
-    _front_sensor = micros();
+    _front_sensor = (long)micros() - _start_time;
 }
 
 FastPing* FastPing::getDefaultInstance(){
     return _instance;
+}
+
+uint8_t FastPing::greatest() {
+    if(_front_sensor > _left_sensor){
+        if(_front_sensor > _right_sensor){
+            return FRONT;
+        }
+        return RIGHT;
+    }else{
+        if(_left_sensor > _right_sensor){
+            return LEFT;
+        }
+        return RIGHT;
+    }
 }
--- src/fast_ping.h
+++ src/fast_ping.h
@@ -1,18 +1,27 @@
 #include <NewPing.h>
 
 #define TIME_OUT 1000000
+#define KEEP_OUT_TIME 1000
+
+enum direction_t{
+    U_TURN = 0,
+    FRONT = 1,
+    LEFT = 1 << 1,
+    RIGHT = 1 << 2,
+};
 
 class FastPing{
     private:
-    unsigned long _left_sensor, _right_sensor, _front_sensor;
-    unsigned long _start_time;
-    uint8_t _left_pin, _right_pin, _front_pin, _trigger;
+    long _left_sensor{}, _right_sensor{}, _front_sensor{};
+    long _start_time{};
+    uint8_t _trigger;
     static FastPing* _instance;
     void pingAll();
 
     public:
     FastPing(uint8_t trigger, uint8_t left_echo_pin, uint8_t right_echo_pin, uint8_t front_echo_pin);
-    void waitForAll();
+    uint8_t waitForAll();
+    uint8_t greatest();
     void recv_left();
     void recv_right();
     void recv_front();
